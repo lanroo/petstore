e9f3e3e5a137ce24f77304237f8d0c1474b13965
lanroo <yladacs@gmail.com>
2025-09-10 11:32:18 -0300

    feat(router): define home and update links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/pets/pets.module').then(m => m.PetsModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
