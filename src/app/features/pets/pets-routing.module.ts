aaae1a5e69935aaba54815be1c06d9272a87d230
lanroo <yladacs@gmail.com>
2025-09-10 11:59:44 -0300

    feat(routing): configurar rotas dos pets e modules

--- src/app/features/pets/pets-routing.module.ts
+++ src/app/features/pets/pets-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PetListComponent } from './pages/pet-list/pet-list.component';
+import { PetDetailComponent } from './pages/pet-detail/pet-detail.component';
+import { PetFormComponent } from './pages/pet-form/pet-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PetListComponent
+  },
+  {
+    path: 'new',
+    component: PetFormComponent
+  },
+  {
+    path: ':id',
+    component: PetDetailComponent
+  },
+  {
+    path: ':id/edit',
+    component: PetFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/pets/pets.module.ts
+++ src/app/features/pets/pets.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { PetsRoutingModule } from './pets-routing.module';
 import { PetListComponent } from './pages/pet-list/pet-list.component';
 import { PetDetailComponent } from './pages/pet-detail/pet-detail.component';
 import { PetFormComponent } from './pages/pet-form/pet-form.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +19,12 @@ import { PetFormComponent } from './pages/pet-form/pet-form.component';
   ],
   imports: [
     CommonModule,
-    PetsRoutingModule
+    PetsRoutingModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class PetsModule { }
